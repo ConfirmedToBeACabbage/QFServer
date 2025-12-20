c24c654478b95f04cda96ec346a92202562f7c83
Simon Bogdanov <simon.bogdanovg@gmail.com>
2025-12-19 22:54:25 -0800

    Restructure: I will just be using the server to do everything. We really need only one server. It's either open or closed to listening or open or closed to sending

--- listener/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/QFServer/listener
-
-go 1.25.5
--- server/server.go
+++ server/server.go
@@ -1,80 +1,29 @@
-package Server
+package server
+
+// TODO:
+// This needs to all be in a structure and as a singleton
+// Also have shutdown methods etc
+// The shutdown would be called when the maintain channel closes
+// Startup method starts from the start method
+
+// The listener can act as both the receiver and listener. Should probablyy change the name
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"time"
 )
 
-func createserver(typeS string, doneS chan string) {
-
-	// Setting up pointer
-	server := &http.Server{
-		Addr:           ":8090",
-		Handler:        nil,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	// Handler
-	// http.HandleFunc("/ping", ping)
-	// http.HandleFunc("/session", session)
-
-	if typeS == "receiver" {
-		// Provide handlers for receiver
-	} else {
-		// Provide handlers for sender
-	}
-
-	// Error handling ListenAndServe
-	for {
-		select {
-		case <-doneS:
-			fmt.Println("SERVER: EXITING")
-			return
-		default:
-			if err := server.ListenAndServe(); err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello!")
 }
 
-// Init the server (Type)
-// Options:
-// 1. Sender
-// - Broadcast listen on LAN to find other receivers
-// - Gather and collect receivers
-// 2. Receiver
-// - Listen in for sender requests
-func InitServer(typeS string, workcloseCH chan string) {
+func InitServer() {
+	http.HandleFunc("/", handler)
 
-	// Create the server
-	go createserver(typeS, workcloseCH)
+	fmt.Println(("Listening on port 8080..."))
+	err := http.ListenAndServe(":8080", nil)
 
-	// // Setting up signal for interrupt checking in a goroutine
-	// exitChan := make(chan struct{})
-	// go func() {
-	// 	sigs := make(chan os.Signal, 1)
-	// 	signal.Notify(sigs, os.Interrupt)
-	// 	<-sigs
-
-	// 	fmt.Println("GATEWAY: Received signal to close test gateway")
-	// 	close(exitChan)
-	// }()
-
-	// fmt.Println("GATEWAY: Gateway is setup and awaiting exit signal")
-
-	// // Awaiting the channel to exit
-	// <-exitChan
-
-	// // Context for exiting (This context visually is like surrounding the function it's a part of. It provides context on what to do. Defer lets you finish the context workflow.)
-	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	// defer cancel()
-	// if err := server.Shutdown(ctx); err != nil {
-	// 	log.Fatal(err)
-	// }
-
+	if err != nil {
+		fmt.Println("Error in starting the server", err)
+	}
 }
